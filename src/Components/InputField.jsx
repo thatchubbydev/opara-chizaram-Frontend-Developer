77d2163b97d663b01e688e5e72adc1a32f893540
Chizaram Opara <chineduelusanmi@gmail.com>
2023-03-07 16:02:37 +0100

    completed form component

--- src/Components/InputField.jsx
+++ src/Components/InputField.jsx
@@ -5,27 +5,21 @@ import CardContainer from "./CardContainer";
 import Capsules from "./Capsules";
 
 const InputField = () => {
-  const [search, setSearch] = useState("");
   const [capsules, setCapsules] = useState([]);
-  const [searchQuery, setSearchQuery] = useState("");
+  const [search, setSearch] = useState("");
   const [statusFilter, setStatusFilter] = useState("");
   const [launchFilter, setLaunchFilter] = useState("");
   const [typeFilter, setTypeFilter] = useState("");
 
   useEffect(() => {
-    async function fetchData() {
-      try {
-        const { data } = await axios.get(
-          "https://api.spacexdata.com/v3/capsules"
-        );
-        setCapsules(data);
-        console.log(capsules);
-      } catch (error) {
-        console.error(error);
-      }
-    }
+    const fetchCapsules = async () => {
+      const response = await axios.get(
+        "https://api.spacexdata.com/v3/capsules"
+      );
+      setCapsules(response.data);
+    };
 
-    fetchData();
+    fetchCapsules();
   }, []);
 
   const handleSearch = (event) => {
@@ -45,62 +39,108 @@ const InputField = () => {
   };
 
   const filteredCapsules = capsules.filter((capsule) => {
-    const capsuleNameMatch = capsule?.capsule_serial
+    const matchesSearch = capsule.capsule_serial
       .toLowerCase()
       .includes(search.toLowerCase());
-    const statusMatch =
+    const matchesStatusFilter =
       !statusFilter ||
       capsule.status.toLowerCase() === statusFilter.toLowerCase();
-    const launchMatch =
+    const matchesLaunchFilter =
       !launchFilter ||
-      capsule.original_launch.toLowerCase() === launchFilter.toLowerCase();
-    const typeMatch =
+      (capsule.original_launch &&
+        capsule.original_launch.toLowerCase() === launchFilter.toLowerCase());
+    const matchesTypeFilter =
       !typeFilter || capsule.type.toLowerCase() === typeFilter.toLowerCase();
-    return capsuleNameMatch && statusMatch && launchMatch && typeMatch;
+
+    return (
+      matchesSearch &&
+      matchesStatusFilter &&
+      matchesLaunchFilter &&
+      matchesTypeFilter
+    );
   });
 
   return (
     <>
-      <div className="mt-[4em] md:mt-10 text-white space-y-2   pl-12 pr-12 pt-6 py-12">
-        <p className="hidden md:block font-IBMPlex">Check out our Rockets!!</p>
-        <div className="flex justify-between flex-col md:flex-row">
-          <div className="space-x-2 flex w-full md:w-1/2 ">
+      <div className="mt-[4em] md:mt-10 text-white input-wrapper space-y-2 pl-12 pr-12 pt-6 py-12 flex">
+        <div>
+          <p className="hidden md:block font-medium text-4xl text-center w-md">
+            Check out our Capsules!!
+          </p>
+        </div>
+
+        <div className="flex justify-center w-full flex-col ">
+          {/* textbox and button container */}
+          <div className="flex w-full md:w-1/2 mx-auto">
             <input
-              className=" w-full rounded-md p-2 border-none text-black mb-6"
-              type="text"
+              className=" w-full input-left-div rounded-md p-2 border-none text-black mb-6"
               placeholder="Search Capsules..."
+              id="search"
+              type="text"
+              value={search}
               onChange={handleSearch}
             />
-            <div className=" bg-gradient-to-r from-cyan-500 to-blue-500 rounded-md p-2 px-2 h-fit ">
+            <div className=" bg-gradient-to-r from-cyan-500 to-blue-500 rounded-full p-4 -mt-2 -ml-4 px-4 h-fit ">
               <AiOutlineSearch size={25} />
             </div>
           </div>
-
-          <div className="space-x-3 ">
-            <button
-              className="bg-gradient-to-r from-[#004C99] via-[#3385FF] to-[#66B2FF] rounded-md p-2 px-4"
-              onClick={handleStatusFilter}
+          {/* button container */}
+          <div className="space-x-3 justify-center mx-auto text-[#03020b]">
+            <select
+              id="status"
+              value={statusFilter}
+              onChange={handleStatusFilter}
+              className="rounded-full p-2 px-4"
             >
-              Status
-            </button>
-            <button
-              className="bg-gradient-to-r from-[#004C99] via-[#3385FF] to-[#66B2FF] rounded-md p-2 px-4"
-              onClick={handleLaunchFilter}
+              <option value="">All</option>
+              <option value="active">Active</option>
+              <option value="retired">Retired</option>
+            </select>
+            <select
+              id="launch"
+              value={launchFilter}
+              className="rounded-full p-2 px-4"
+              onChange={handleLaunchFilter}
             >
-              Launch
-            </button>
-            <button
-              className="bg-gradient-to-r from-[#004C99] via-[#3385FF] to-[#66B2FF] rounded-md p-2 px-4"
-              onClick={handleTypeFilter}
+              <option value="">All</option>
+              {capsules.map((capsule) => (
+                <option
+                  key={capsule.capsule_serial}
+                  value={capsule.original_launch}
+                >
+                  {capsule.original_launch}
+                </option>
+              ))}
+            </select>
+            <select
+              id="type"
+              value={typeFilter}
+              className="rounded-full p-2 px-4"
+              onChange={handleTypeFilter}
             >
-              Type
-            </button>
+              <option value="">All</option>
+              {capsules.map((capsule) => (
+                <option key={capsule.capsule_serial} value={capsule.type}>
+                  {capsule.type}
+                </option>
+              ))}
+            </select>
           </div>
         </div>
       </div>
       <div className="pl-12 pt-2 pr-12">
+        <ul className="text-white">
+          {filteredCapsules.map((capsule) => (
+            <li key={capsule.capsule_serial}>
+              <h2>{capsule.capsule_serial}</h2>
+              <p>Status: {capsule.status}</p>
+              <p>Original launch: {capsule.original_launch}</p>
+              <p>Type: {capsule.type}</p>
+            </li>
+          ))}
+        </ul>
         {/* <CardContainer capsules={filteredCapsules} /> */}
-        <Capsules />
+        {/* <Capsules /> */}
       </div>
     </>
   );
